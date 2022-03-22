7e251950d54d30f4e7261845fa88180588408c08
Fariz5 <iqball.frz@gmail.com>
2022-03-22 07:33:02 +0700

    Update Api Config

--- app/src/main/java/com/izo/apigithubuserapp/api/ApiConfig.kt
+++ app/src/main/java/com/izo/apigithubuserapp/api/ApiConfig.kt
@@ -12,8 +12,11 @@ class ApiConfig {
 
 
         fun getApiService(): ApiService {
-            val loggingInterceptor =
+            val loggingInterceptor = if(BuildConfig.DEBUG) {
                 HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+            } else {
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+            }
 
             val client = OkHttpClient.Builder()
                 .addInterceptor(loggingInterceptor)
